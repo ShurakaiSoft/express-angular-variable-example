289c6c843a522a68b466fef85c3a23cbb00bf3d6
Stephen Parker <shurakaisoft@gmail.com>
2014-03-10 22:04:20 +1100

    Removed controllers from global scope

--- client/js/core.js
+++ client/js/core.js
@@ -3,15 +3,16 @@
  *
  */
 
-function JavaScriptGlobalCtrl($scope) {
-	$scope.users = userList;
-}
 
-function RestfulJsonCtrl($scope, $http) {
+var demo = angular.module('demo', []);
+
+demo.controller('JavaScriptGlobalCtrl', function ($scope) {
+	$scope.users = userList;
+});
+
+demo.controller('RestfulJsonCtrl', function ($scope, $http) {
 	$http.get('/api/users')
 	.success(function (data) {
 		$scope.users = data;
 	});
-}
-
-angular.module('demo', []);
+});
